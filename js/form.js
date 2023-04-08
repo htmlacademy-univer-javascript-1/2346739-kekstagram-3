92ba1b6774dbd92dd7ec94f97758eb0aca41e3ae
MeeLeSh <teadorWOT@gmail.com>
2023-04-08 14:53:44 +0300

    module 11

--- js/form.js
+++ js/form.js
@@ -1,176 +1,55 @@
-import { strSizeCheck, isEscapeKey } from './utils.js';
-import { MAX_STRING_LENGTH, MAX_HASHTAG_COUNT, MAX_HASHTAG_LENGTH, ErrorMessage } from './consts.js';
+import { onFormInput, resetForm } from './form-validation.js';
+import { isEscKey as isEscKey } from './utils.js';
 import { setDefaultScale } from './scale.js';
 import { setDefaultEffect } from './effects.js';
 
-const body = document.querySelector('body');
-const submitBtn = document.querySelector('.img-upload__submit');
+
 const imgUploadField = document.querySelector('#upload-file');
 const form = document.querySelector('.img-upload__form');
 const editImg = document.querySelector('.img-upload__overlay');
 const closeBtn = form.querySelector('.img-upload__cancel');
-const hashtagsField = form.querySelector('.text__hashtags');
-const commentsField = form.querySelector('.text__description');
 
-const pristine = new Pristine(form, {
-  classTo: 'img-upload__field-wrapper',
-  errorTextParent: 'img-upload__field-wrapper',
-  errorTextClass: 'img-upload__error-text'
-});
 
-const closeUploadPopup  = () => {
+export const closeUploadPopup = () => {
   editImg.classList.add('hidden');
-  body.classList.remove('modal-open');
-  form.reset();
+  document.body.classList.remove('modal-open');
+
+  imgUploadField.value = '';
+  form.querySelector('.text__hashtags').value = '';
+  form.querySelector('.text__description').value = '';
+  resetForm();
+
+  form.removeEventListener('submit', onFormInput);
 };
 
-const onBtnEscKeydown = (evt) => {
-  if (isEscapeKey(evt)) {
-    closeUploadPopup();
-    document.removeEventListener('keydown', onBtnEscKeydown);
-  }
-};
-
-const onCloseBtnClick = () => {
+const onCloseClick = () => {
   closeUploadPopup();
-  document.removeEventListener('keydown', onBtnEscKeydown);
+  closeBtn.removeEventListener('click', onCloseClick);
 };
 
-const addFieldListeners = (field) => {
-  field.addEventListener('focus', () => {
-    document.removeEventListener('keydown', onBtnEscKeydown);
-  });
-  field.addEventListener('blur', () => {
-    document.addEventListener('keydown', onBtnEscKeydown);
-  });
-};
+const onClosingBtnClick = () => onCloseClick();
 
-const btnAdjustment = () => {
-  submitBtn.disabled = !pristine.validate();
-};
+const isNotTarget = (evt) => !evt.target.classList.contains('text__hashtags')
+  && !evt.target.classList.contains('text__description');
 
-const onImgUploadFieldchange = () => {
-  editImg.classList.remove('hidden');
-  body.classList.add('modal-open');
-  closeBtn.addEventListener('click', onCloseBtnClick);
-  document.addEventListener('keydown',onBtnEscKeydown);
-  addFieldListeners(commentsField);
-  addFieldListeners(hashtagsField);
-  btnAdjustment();
-};
-
-const getUniqueHashtags = (hashtags) => {
-  const uniqueSet = new Set(hashtags);
-  return hashtags.length === uniqueSet.size;
-};
-
-let errorMessage = '';
-
-const error = () => errorMessage;
-
-const hashtagsHandler = (string) => {
-  errorMessage = '';
-
-  const inputText = string.toLowerCase().trim();
-
-  if(!inputText) {
-    return true;
+const onDocumentEscKeyDown = (evt) => {
+  if (isEscKey(evt) && isNotTarget(evt)) {
+    onCloseClick();
+    document.removeEventListener('keydown', onDocumentEscKeyDown);
   }
-
-  const inputHashtags = inputText.split(/\s+/);
-
-  if(inputHashtags.length === 0) {
-    return true;
-  }
-
-  const rules = [
-    {
-      check: inputHashtags.some((item) => item.indexOf('#', 1) >= 1),
-      error: ErrorMessage.SEPARETED_BY_SPASES,
-    },
-
-    {
-      check: inputHashtags.length > MAX_HASHTAG_COUNT,
-      error: ErrorMessage.MAX_COUNT_HASHTAG,
-    },
-
-    {
-      check: inputHashtags.some((item) => item[0] !== '#'),
-      error: ErrorMessage.START_WITH,
-    },
-
-    {
-      check: inputHashtags.some((item) => item.length > MAX_HASHTAG_LENGTH),
-      error: ErrorMessage.HASHTAG_MAX_LENTH,
-    },
-
-    {
-      check: inputHashtags.some((item) => !/^#[A-Za-zА-Яа-яЁё0-9]{1,19}$/.test(item)),
-      error: ErrorMessage.UNACCEPTABLE_SYMBOLS,
-    },
-
-    {
-      check: !getUniqueHashtags(inputHashtags),
-      error: ErrorMessage.NO_REPEAT,
-    },
-  ];
-
-  return rules.every((rule) => {
-    const isInvalid = rule.check;
-    if(isInvalid) {
-      errorMessage = rule.error;
-    }
-    return !isInvalid;
-  });
 };
 
-const commentHandler = (string) => {
-  errorMessage = '';
-
-  const inputText = string.trim();
-
-  if(!inputText) {
-    return true;
-  }
-
-  const rule = {
-    check: !strSizeCheck(inputText, MAX_STRING_LENGTH),
-    error: ErrorMessage.COMMENT_MAX_LENGTH,
-  };
-
-  const isInvalid = rule.check;
-  if(isInvalid) {
-    errorMessage = rule.error;
-  }
-  return !isInvalid;
-};
-
-const validateForm = () => {
-  pristine.addValidator(hashtagsField, hashtagsHandler, error);
-  pristine.addValidator(commentsField, commentHandler, error);
-  btnAdjustment();
-};
-
-const onHashtagInput = () => btnAdjustment();
-
-const onCommentInput = () => btnAdjustment();
-
-const initUploadForm = () => {
-  imgUploadField.addEventListener('change', onImgUploadFieldchange);
-  hashtagsField.addEventListener('input', onHashtagInput);
-  commentsField.addEventListener('input', onCommentInput);
-  validateForm();
-};
-
-const onFileInput = () => {
+const onUploadingFieldInput = () => {
   editImg.classList.remove('hidden');
   document.body.classList.add('modal-open');
-  closeBtn.addEventListener('click', closeUploadPopup);
-  document.addEventListener('keydown', onBtnEscKeydown);
-  form.addEventListener('submit', adjustBtn);
+  closeBtn.addEventListener('click', onClosingBtnClick);
+  document.addEventListener('keydown', onDocumentEscKeyDown);
+  form.addEventListener('submit', onFormInput);
 
   setDefaultScale();
   setDefaultEffect();
 };
 
-export { initUploadForm, onFileInput };
+imgUploadField.addEventListener('input', onUploadingFieldInput);
+
+export { onDocumentEscKeyDown };
--- js/main.js
+++ js/main.js
@@ -1,7 +1,30 @@
-import {draw} from './draw.js';
+import { renderPhotos } from './pictures.js';
+import { sendRequest } from './fetch.js';
+import './big-picture.js';
+import './form.js';
+import './form-validation.js';
+import './effects.js';
+import './scale.js';
 
-draw()
+let photos = [];
 
-import {initUploadForm} from './form.js';
+const onSuccess = (data) => {
+  photos = data.slice();
+  renderPhotos(photos);
+};
 
-initUploadForm();
+const onFail = () => {
+  const messageAlert = document.createElement('div');
+  messageAlert.style.position = 'absolute';
+  messageAlert.style.left = 0;
+  messageAlert.style.top = 0;
+  messageAlert.style.right = 0;
+  messageAlert.style.textAlign = 'center';
+  messageAlert.style.fontSize = '30px';
+  messageAlert.style.backgroundColor = 'red';
+  messageAlert.style.padding = '10px 5px';
+  messageAlert.textContent = 'Ошибка загрузки данных';
+  document.body.append(messageAlert);
+};
+
+sendRequest(onSuccess, onFail, 'GET');
