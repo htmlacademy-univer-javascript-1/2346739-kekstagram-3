64b15a2a1fc330be6585d06060b5931bda210e9a
MeeLeSh <teadorWOT@gmail.com>
2023-04-13 20:19:12 +0300

    fix utils

--- js/utils.js
+++ js/utils.js
@@ -1,6 +1,6 @@
 export function getRandomInteger(min, max) {
-    // взял отсюда https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/random
-    return (Math.random() * (max - min) + min) | 0;
+  // взял отсюда https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/random
+  return (Math.random() * (max - min) + min) | 0;
 }
 
 export function strSizeCheck(str, max) {
